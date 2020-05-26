8266e7584e20b50ebf516084c6ba54e506463086
RykerLe <hpcqt97@gmail.com>
2020-05-26 23:19:27 +0700

    refactor, add logger

--- app/controller/chat.go
+++ app/controller/chat.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 	"github.com/phuonglvh/golang-first-pet/app/model"
+	"github.com/phuonglvh/golang-first-pet/util/logger"
 )
 
 // ChatHandler handle the chat feature
@@ -30,23 +31,19 @@ var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBuffer
 func (chat *ChatHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	socket, err := upgrader.Upgrade(w, req, nil)
 	if err != nil {
-		log.Fatal("ServeHTTP:", err)
+		logger.Error.Fatalf("Chat websocket has encountered error while serving http: %s", err)
 		return
 	}
 
 	vars := mux.Vars(req)
 	cookie, _ := req.Cookie("X-Authorization")
 	if err != nil {
-		log.Println(err.Error())
-	} else {
-		log.Println(cookie.Value)
+		logger.Error.Println(err.Error())
 	}
-	clientId := cookie.Value
-	log.Println("number of rooms: ", len(chat.Rooms))
-
+	clientID := cookie.Value
 	var roomID string = vars["id"]
 	if len(roomID) < 1 {
-		log.Println("path param 'id' is missing")
+		logger.Error.Println("Query param 'id' is missing")
 		return
 	}
 
@@ -54,25 +51,24 @@ func (chat *ChatHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	if room == nil {
 		room = model.NewRoom(roomID)
 		chat.Rooms[roomID] = room
-		log.Printf("create a new room: %s -> total rooms: %d ", room.ID, len(chat.Rooms))
+		log.Printf("Add new room: %s to list of rooms", room.ID)
+		logger.Trace.Printf("Number of rooms: %d", len(chat.Rooms))
 		go room.Run()
-	} else {
-		log.Printf("room is existing: %s", room.ID)
 	}
 	client := &model.Client{
-		ID:     clientId,
+		ID:     clientID,
 		Socket: socket,
 		Send:   make(chan []byte, messageBufferSize),
 		Room:   room,
 	}
 
 	if client == nil {
-		log.Fatal("error while attempting to create a new client")
+		logger.Error.Fatal("Error while attempting to create a new client")
 	} else {
-		log.Printf("Client joins room: %s", room.ID)
+		logger.Info.Printf("Client %s has joined the room %s", client.ID, room.ID)
 		room.Join <- client
 		defer func() {
-			log.Printf("Client left room: %s", room.ID)
+			logger.Info.Printf("Client %s has left the room %s", client.ID, room.ID)
 			room.Leave <- client
 		}()
 		go client.Write()
@@ -80,24 +76,3 @@ func (chat *ChatHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 
 }
-
-// func main() {
-// 	var addr = flag.String("addr", ":8080", "The addr of the application.")
-// 	flag.Parse() // parse the flags
-
-// 	r := model.NewRoom()
-// 	r.Tracer = trace.New(os.Stdout)
-
-// 	http.Handle("/", &model.TemplateHandler{Filename: "chat.html"})
-// 	http.Handle("/room", r)
-
-// 	// get the room going
-// 	go r.Run()
-
-// 	// start the web server
-// 	log.Println("Starting web server on", *addr)
-// 	if err := http.ListenAndServe(*addr, nil); err != nil {
-// 		log.Fatal("ListenAndServe:", err)
-// 	}
-
-// }
