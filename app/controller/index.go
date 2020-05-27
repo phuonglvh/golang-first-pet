0ba48a88857be168d8902011bdc31b6fd6e09775
RykerLe <hpcqt97@gmail.com>
2020-05-27 22:46:41 +0700

    gen random qrcode string

--- app/controller/index.go
+++ app/controller/index.go
@@ -2,9 +2,17 @@ package controller
 
 import (
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 // HomeHandler handle index page
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "qrcode/generator", http.StatusTemporaryRedirect)
+	http.Redirect(w, r, genRedirectURI(), http.StatusTemporaryRedirect)
+}
+
+func genRedirectURI() string {
+	uuid := uuid.New().String()
+	uuid = uuid[len(uuid)-5:]
+	return "qrcode/generator?string=" + uuid
 }
