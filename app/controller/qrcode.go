e543b3cec6bcc7757e15ab6c0ee16e2433d289f3
Phuong Le <hoangphuongle1997@gmail.com>
2020-05-28 08:25:01 +0700

    generate random room Id for QRcode

--- app/controller/qrcode.go
+++ app/controller/qrcode.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/boombuler/barcode"
 	"github.com/boombuler/barcode/qr"
+	"github.com/google/uuid"
 	util "github.com/phuonglvh/golang-first-pet/util/ip"
 )
 
@@ -15,6 +16,12 @@ func ViewCodeHandler(w http.ResponseWriter, r *http.Request) {
 	scheme := util.GetScheme(r)
 	hostname := util.GetMyIP()
 	dataString := r.URL.Query().Get("string")
+
+	if dataString == "" {
+		uuid := uuid.New().String()
+		dataString = uuid[len(uuid)-5:]
+	}
+
 	data := scheme + "://" + hostname + ":8080" + "/chat/rooms/" + dataString
 	fmt.Println(data)
 
