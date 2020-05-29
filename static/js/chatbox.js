0ca261809d399c664d908ac16565e767511b866e
Phuong Le <hoangphuongle1997@gmail.com>
2020-05-29 16:03:02 +0700

    load old message, display messages

--- static/js/chatbox.js
+++ static/js/chatbox.js
@@ -12,22 +12,23 @@ const SenderType = {
 $(function () {
   const baseURI = '/chat/rooms/'
   const segments = window.location.pathname.split('/')
-  if(segments.length < 1)
+  if (segments.length < 1)
     return
   const roomId = segments[segments.length - 1]
-  // $('#code').attr('src', `${baseURI}?string=${roomId}`)
+  const userId = window.sessionStorage.getItem('clientId');
+  $('#room_id').text(`Group: ${roomId}`)
 
   const content = (type, body, time) => {
-    return `<div class="msg_cotainer${SenderType[type]}">
+    return `<div class='msg_cotainer${SenderType[type]}'>
               ${body}
-              <span class="msg_time${SenderType[type]}">${time}</span>
+              <span class='msg_time${SenderType[type]}'>${time}</span>
             </div>`;
   }
 
   const avatar = (imgUrl) => {
     return (
-      `<div class="img_cont_msg">
-        <img src="${imgUrl}" class="rounded-circle user_img_msg">
+      `<div class='img_cont_msg'>
+        <img src='${imgUrl}' class='rounded-circle user_img_msg'>
       </div>`
     );
   }
@@ -36,7 +37,7 @@ $(function () {
     var statEnd = 'start';
     type === SenderType.mine ? statEnd = 'end' : 'start'
     return `
-          <div class="d-flex justify-content-${statEnd} mb-4">
+          <div class='d-flex justify-content-${statEnd} mb-4'>
            ${content(type, body, time)}
             ${avatar(imgUrl)}
           </div>
@@ -45,6 +46,52 @@ $(function () {
 
   const uri = `${baseURI}/${roomId}/messages`
   $.get(uri, (data, status) => {
-    console.log({ data, status });
+    const messages = JSON.parse(data);
+    const items = []
+    for (const msg of messages) {
+      const item = generateMessage(
+        msg.Sender === userId,
+        'http://www.gravatar.com/avatar',
+        msg.content,
+        new Date(parseInt(msg.Timestamp)).toString()
+      )
+      items.push(item);
+      // $('#msg_container').append(item)
+    }
+    console.log({ items})
+    $('#room_total_messages').text(`${items.length} messages`)
+    $('#msg_container').append(items)
   });
 })
+
+const generateMessage = (isMine, avatarUrl, content, time) => {
+  return isMine ? myMessage(avatarUrl, content, time) : theirMessage(avatarUrl, content, time)
+}
+
+const theirMessage = (avatarUrl, content, time) => {
+  return (
+    `<div class='d-flex justify-content-start mb-4'>
+      <div class='img_cont_msg'>
+        <img src='${avatarUrl}' class='rounded-circle user_img_msg'>
+      </div>
+      <div class='msg_cotainer'>
+        ${content}
+        <!-- <span class='msg_time'>${time}</span> -->
+      </div>
+    </div>`
+  );
+}
+
+const myMessage = (avatarUrl, content, time) => {
+  return (
+    `<div class='d-flex justify-content-end mb-4'>
+      <div class='msg_cotainer_send'>
+        ${content}
+        <!-- <span class='msg_time_send'>${time}</span> -->
+      </div>
+      <div class='img_cont_msg'>
+        <img src='${avatarUrl}' class='rounded-circle user_img_msg'>
+      </div>
+    </div>`
+  );
+}
